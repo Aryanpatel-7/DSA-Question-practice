f36a42cf6c0eedecc9fafce0bfe1e102e05ebfcb
Aryanji Patel <ap0905749@gmail.com>
2026-03-22 13:24:07 +0530

    Learned insertion Sort and implemented

---
+++ h) Sorting/Q3_insertionSort.cpp
@@ -0,0 +1,31 @@
+// insertion sort
+#include<iostream>
+using namespace std;
+
+void  insertionSort(int arr[],int n){
+  for (int i = 1; i < n; i++){
+    int curr = arr[i];
+    int prev = i-1;
+
+    while (prev >=0 && arr[prev] > curr) {
+        arr[prev+1] = arr[prev];
+        prev --;
+    }
+      arr[prev+1] = curr;
+  }
+}
+
+void printArray(int arr[],int n){
+  for (int i = 0; i < n; i++)
+  {
+    cout<<arr[i]<<" ";
+  }
+}
+int main (){
+  int arr []= {8,4,1,5,2,6,3,7};
+  int n = sizeof(arr)/sizeof(int);
+
+  insertionSort(arr,n);
+ printArray(arr,n);
+  return 0;
+}
