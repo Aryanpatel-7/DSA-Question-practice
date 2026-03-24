73f78600d00b63b92552bdaaec40797904c4f7ba
Aryanji Patel <ap0905749@gmail.com>
2026-03-24 16:59:03 +0530

    Solve second largest number  problem

---
+++ b) Array/Q7_secondLargestElement.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main (){
+  int arr [] ={1,2,4,7,7,5};
+  int n =sizeof(arr)/sizeof(int);
+  
+  int largest =INT_MIN;
+  int second_largest =INT_MIN;
+
+  for (int i = 0; i < n; i++) {
+    if (arr[i] > largest) {
+      second_largest =largest;
+      largest = arr[i];
+    }
+   else if( arr[i] >second_largest && arr[i] != largest){
+      second_largest = arr[i];
+      cout<<"second largest is"<<" "<<second_largest<<endl;
+    
+   }
+  
+  }
+   return 0;
+ 
+}
