e103a1275a37775807334afee252892b615ce4c5
Aryanji Patel <ap0905749@gmail.com>
2026-03-25 18:20:40 +0530

    Solved Left rotate array

---
+++ b) Array/Q10_leftRotate.cpp
@@ -0,0 +1,20 @@
+// left rotate the array one
+#include <iostream>
+using namespace std;
+int main (){
+  int arr []={1,2,3,4,5};
+  int n =sizeof(arr)/sizeof(int);
+
+  int temp =arr[0];
+  for (int i = 0; i < n-1; i++) {
+    arr[i]=arr[i+1];
+  }
+  arr[n-1] = temp;
+  for (int i = 0; i <n; i++)
+  {
+   cout<< arr[i]<<" ";
+  }
+  
+  
+  return 0;
+}
