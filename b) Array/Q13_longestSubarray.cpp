ba3b561e59093c802df16de4f18167e9c3db5a7e
Aryanji Patel <ap0905749@gmail.com>
2026-03-28 19:24:34 +0530

    Solved longest subarray

---
+++ b) Array/Q13_longestSubarray.cpp
@@ -0,0 +1,23 @@
+// longest subarray with given sum k
+#include <iostream>
+#include<climits>
+using namespace std;
+int main (){
+  int arr[]={10,5,2,7,1,9};
+  int n =sizeof(arr)/sizeof(int);
+  int k =15;
+  int maxlen = 0;
+  for (int i = 0; i < n; i++) {
+    int sum =0;
+
+  for (int j = i; j < n; j++) {
+    sum +=arr[j];
+    if (sum == k)  {
+     maxlen =max(maxlen,j-i+1);
+    } 
+  }
+  }
+  cout<<maxlen<<" ";
+  
+  return 0;
+}
