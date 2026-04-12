77ffe268edd05a2dc0bae572a732025b841409d2
Aryanji Patel <ap0905749@gmail.com>
2026-04-12 19:21:50 +0530

    Solved LeetCode 1539 :Kth Missing Positive Number

---
+++ g) BinarySearchAlgo/Q14_KthMissingpos.cpp
@@ -0,0 +1,33 @@
+//  
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int missingpos(int nums[],int n,int k){
+  int st =0 ,  end =n-1;
+  while(st <= end){
+    int mid = st +(end-st)/2;
+    int missingval = nums[mid] -(mid+1);
+
+        if(missingval < k){
+             st = mid+1;
+        } else {
+            end = mid-1;
+        }
+    }
+
+      return k + end +1;
+  }
+
+
+
+int main (){
+
+int nums []= {2,3,4,7,11};
+  int n = sizeof(nums)/sizeof(int);
+  int k =5;
+  
+ cout<<"Kth missing position  is :"<<missingpos(nums,n,k);
+  return 0;
+}
