2d2fc8e42f2d7eb5510d0f9a06c9280541301141
Aryanji Patel <ap0905749@gmail.com>
2026-04-13 11:18:30 +0530

    Solved LeetCode 1910 : Remove All Occurrences of a Substring

---
+++ i) String/Q2_removeAll.cpp
@@ -0,0 +1,17 @@
+//Remove All Occurrences of a Substring (leetcode 1910)
+
+#include <iostream>
+using namespace std;
+
+int main (){
+  string s ="daabcbaabcbc" , part ="abc";
+
+  while (s.length() > 0 && s.find(part) < s.length()) {
+    s.erase(s.find(part), part.length());
+    
+  }
+  cout<<s;
+
+
+  return 0;
+}
