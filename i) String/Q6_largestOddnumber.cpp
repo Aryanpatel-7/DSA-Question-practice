9666d614395e92e9ae947a0c47688a219836e374
Aryanji Patel <ap0905749@gmail.com>
2026-04-15 11:21:50 +0530

    Solved LeetCode 1903 :Largest Odd Number in String

---
+++ i) String/Q6_largestOddnumber.cpp
@@ -0,0 +1,30 @@
+// Largest Odd Number in String (leetcode Q1903)
+
+#include <iostream>
+#include <vector>
+#include<string>
+using namespace std;
+
+int main (){
+string s ="5347";
+int ind =-1;
+for(int i =s.length()-1; i>=0; i--){ //last odd val
+  if((s[i]-'0')%2==1){
+    ind =i;
+    break;
+  }
+}   
+if(ind == -1) {
+  cout<<"";
+  
+}
+
+  int i =0;
+  while (i < ind && s[i] == '0') {
+     i++;
+  }
+  cout<<"largest odd number is :"<< s.substr(i, ind-i+1);
+  
+
+return 0;
+}
