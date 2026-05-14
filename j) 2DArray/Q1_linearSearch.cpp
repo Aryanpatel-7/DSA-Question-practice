f3c909324224b9f6333ca2fc68b90dd69880a801
Aryanji Patel <ap0905749@gmail.com>
2026-05-14 13:08:28 +0530

    linearSearch in 2D array

---
+++ j) 2DArray/Q1_linearSearch.cpp
@@ -0,0 +1,27 @@
+// linearSearch in 2D array 
+# include <iostream>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+bool linearSearch(int mat[][3], int row, int col, int key ){
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      if(mat[i][j] == key){
+        return true;
+      }
+  }  
+  } 
+   return false;
+}
+
+ int main (){
+  int matrix[4][3]={{1,2,3},{4,5,6},{7,8,9}};
+  int row =4;
+  int col =3;
+
+ cout<<linearSearch(matrix, row, col, 8);
+
+
+return 0;
+}
