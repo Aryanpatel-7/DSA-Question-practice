8b2679aa65cf2414419d70a93dfdaf78ec82d1c0
Aryanji Patel <ap0905749@gmail.com>
2026-05-23 17:36:47 +0530

    Solved LeetCode 240 : Search a 2D Matrix II

---
+++ j) 2DArray/Q5_Search2DMatrixII.cpp
@@ -0,0 +1,35 @@
+ // Search a 2D Matrix II (leetcode Q240)
+
+ #include <iostream>
+#include <vector>
+using namespace std;
+
+ bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int m = mat.size(),  n = mat[0].size();
+
+     int r =0 ,  c =n-1;
+     while(r < m && c >=0){
+    if(target == mat[r][c]){
+        return true;
+    } else if(target < mat[r][c]){
+        c--;
+    } else {
+        r++;
+    }
+
+     }
+      return false;
+    }
+
+int main() {
+
+    vector<vector<int>> matrix = {
+        {1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22}
+    };
+
+    int target = 5;
+
+    cout << searchMatrix(matrix, target);
+
+    return 0;
+}
