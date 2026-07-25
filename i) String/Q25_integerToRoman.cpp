1219ef8ff8c04b656fce97588d595e9b125b32e8
Aryanji Patel <ap0905749@gmail.com>
2026-07-25 14:21:39 +0530

    Solve LeetCode 12: Integer to Roman

---
+++ i) String/Q25_integerToRoman.cpp
@@ -0,0 +1,39 @@
+//   Integer to Roman (leetcode Q12)
+#include<iostream>
+using namespace std ;
+
+
+int main (){
+  int num =3749;
+
+  string ans =""; 
+   int values[] = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        string symbols[] = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
+        int index =0;
+
+        while(num >0){
+
+            if(num >= values[index]){
+                ans+=symbols[index];
+
+                num-=values[index];
+            }
+            else{
+                index++;
+            }
+
+        }
+        cout<< ans;
+
+  return 0;
+}
