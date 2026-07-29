5234214b5800072ce57acf7a7722506e4e3912d6
Aryanji Patel <ap0905749@gmail.com>
2026-07-29 14:03:29 +0530

    longest prefix sufix (gfg)

---
+++ i) String/Q26_longestPrefixSufix.cpp
@@ -0,0 +1,35 @@
+// longest prefix sufix
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+
+int main()
+{
+  string s = "aabcdaabc";
+  vector<int>lps(s.size(),0);
+  int pre=0, suf=1;
+
+  while(suf <s.size()){
+
+    if(s[pre]==s[suf]){
+      lps[suf]= pre+1;
+      pre++,suf++;
+    }
+    else{
+      if(pre==0){
+        lps[suf]=0;
+        suf++;
+      }
+      else{
+        pre=lps[pre-1];
+
+      }
+    }
+  }
+cout<<lps[s.size()-1];
+
+  return 0;
+}
